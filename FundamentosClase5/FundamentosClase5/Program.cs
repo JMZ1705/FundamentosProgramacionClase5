c57d8e04b30fef886e21680318aa03d218a1080c
JMZ1705 <juanmizapata08@gmail.com>
2026-07-28 13:36:35 -0500

    Mi primer commit

---
+++ FundamentosClase5/FundamentosClase5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace FundamentosClase5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el código fuente
+            string nombre= "Ferney Alejandro";
+            Console.WriteLine("Bienvenido "+nombre +" al curso de fundamentos de programación");
+
+        }
+    }
+}
