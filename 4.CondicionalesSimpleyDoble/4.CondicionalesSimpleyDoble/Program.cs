3140cbdcf56cc8048c9e095076c71ab92987b0ee
JMZ1705 <juanmizapata08@gmail.com>
2026-08-06 13:24:41 -0500

    CondiconalesSimplesyDobles

---
+++ 4.CondicionalesSimpleyDoble/4.CondicionalesSimpleyDoble/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace _4.CondicionalesSimpleyDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   //ESTRUCTURAS DE CONTROL CONDICIONALES
+               //SIMPLE
+               *//*  //Crear un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: "Bievenido a mi sitio web"
+                 byte edad = 0;
+                 Console.WriteLine("Ingrese su edad");
+                 edad = Convert.ToByte( Console.ReadLine()); // La consola lee todo lo que le escriban directamente como string, toca pasarlo a byte
+                 if (edad >= 18)
+                 {
+                     Console.WriteLine("Bienvenido a mi sitio web");
+                 }
+                 if (edad <= 18)
+                 {
+                     Console.WriteLine("Malparido Pajero");
+                 }*//*
+               // un programador diciendo q era joda, era byte 
+
+               //Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando ue debe abonar impuestos.
+               int sueldo = 0;
+               string nombre; 
+               Console.WriteLine("Ingrese su nombre");
+               nombre = Console.ReadLine();
+               Console.WriteLine("Ingrese su sueldo");
+               sueldo = Convert.ToInt32(Console.ReadLine());
+               if (sueldo >= 3000)
+               {
+                   Console.WriteLine(nombre + " usted debe abonar impuestos, maldito vago izquierdoso mantenido que quiere todo regalado");
+               }
+               if(sueldo < 3000)
+               {
+                   Console.WriteLine(nombre + " usted está muy pobre, póngase a trabajar, trabajar y trabajar (Pero por ahora no tiene por qué abonar impuestos)");
+               }*/
+            //Crear un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: "Bievenido a mi sitio web", de lo cotrario, que diga el mensaje "No puedes ingresar a mi sitio web."
+            /*byte edad = 0;
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToByte(Console.ReadLine()); // La consola lee todo lo que le escriban directamente como string, toca pasarlo a byte
+            if (edad >= 18)
+            {
+                Console.WriteLine("Bienvenido a mi sitio web"); // si se cumple la condición
+            }
+            else 
+            {
+                Console.WriteLine("Malparido Pajero"); // si no se cumple la condición
+*/
+            }
+        }
+    }
+}
