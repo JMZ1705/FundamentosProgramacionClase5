bc24d8700b6b5b46ce616e0d0daeed6d6790b192
JMZ1705 <juanmizapata08@gmail.com>
2026-08-25 13:34:07 -0500

    ciclowhile

---
+++ 7.While/7.While/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace _7.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciclo While
+            //Crear un algoritmo que permita generar los primeros 5 numero positivos y su sumatoria
+            /*
+                        int contador = 1;
+                        int acumulador = 1;
+
+                        while (contador < 5)
+                        {
+                            contador++;
+                            acumulador = contador + acumulador;
+                        }
+                        Console.WriteLine($"La suma de los 5 primeros numeros positivos es {acumulador}");
+            */
+            //Crear un algoritmo que permita calcular la factorial de un numero dado por el usuario
+
+            int contador = 1;
+            int acumulador = 1;
+         
+            int numero = 0;
+
+            Console.WriteLine("ingrese un numero entero positivo");
+            numero = int.Parse(Console.ReadLine());
+
+            while( contador <= numero)
+            {
+                
+                acumulador = contador * acumulador;
+                contador++;
+            }
+            Console.WriteLine($"El factorial de {numero} es {acumulador}");
+        }
+    }
+}
