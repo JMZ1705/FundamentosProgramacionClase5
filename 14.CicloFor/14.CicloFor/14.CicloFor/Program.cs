615f046101ba208bdd2f1c32bc88ff465e858d67
JMZ1705 <juanmizapata08@gmail.com>
2026-09-15 18:12:17 -0500

    tareas

---
+++ 14.CicloFor/14.CicloFor/14.CicloFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un programa que permita imprimir por pantalla los números múltiplos de 5, el usuario debe ingresar por el teclado el rango de números a evaluar. 
+            int porCinco = 1;
+
+            Console.WriteLine("ingrese el numero de multiplos de 5 que desea");
+            porCinco = int.Parse(Console.ReadLine());
+            for(int multiplo=0; multiplo<=porCinco; multiplo++)
+            {
+                Console.WriteLine($"{multiplo} X 5 = {multiplo*5}");
+            }
+        }
+    }
+}
---
+++ 8.CicloWhile2/8.CicloWhile2/8.CicloWhile2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _8.CicloWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados.
+            int sumNum = 0;
+            int numero = 0;
+            Console.WriteLine("introduzca un numero entero");
+            numero = int.Parse(Console.ReadLine());
+            
+            while (numero > 0) 
+            {
+                sumNum += numero;
+                Console.WriteLine("introduzca otro numero entero");
+                numero = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"la suma de los numeros ingresados es {sumNum}");
+
+        }
+    }
+}
